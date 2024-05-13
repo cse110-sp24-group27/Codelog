3cefad142e9cbe326d414b066a10b4c0ff55db64
shootingdarts <103202799+shootingdarts@users.noreply.github.com>
2024-05-12 21:58:46 -0700

    Moved js files to the correct folder

--- source/assets/scripts/test.js
+++ source/assets/scripts/test.js
@@ -1,1 +1,50 @@
+/**
+ * This is a function.
+ *
+ * @param {string} n - A string param
+ * @param {string} [o] - A optional string param
+ * @param {string} [d=DefaultValue] - A optional string param
+ * @return {string} A good string
+ *
+ * @example
+ *
+ *     foo('hello')
+ */
 
+function foo(n, o, d) {
+  return n
+}
+
+/**
+ * Generate a book description.
+ *
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ *
+ * @returns {string} Description of the book.
+ * @throws An error if values are not set.
+ */
+function Book(title, author) {
+  if (!title || !author) {
+    throw new Error('title and author must be set');
+  }
+  
+  return `${title} by ${author}`;
+}
+
+/**
+ * @param {Bar} x
+ */
+
+function test(x) {}
+
+/**
+ * @param {Bar} y
+ */
+
+function test2(y) {}
+
+/**
+ * @type {number}
+ */
+var FOO = 1
