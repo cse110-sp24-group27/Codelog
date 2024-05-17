ec7f1bdca8d1d3fff2764065cd6c3ac4a3333870
Kristhian Ortiz <147196544+klortiz13@users.noreply.github.com>
2024-05-16 18:14:44 -0700

    finished js for entry journal

--- journal.js
+++ journal.js
@@ -1,10 +1,10 @@
 // When clicked the arrow button, go back to last page
 function goBack () {
-  window.history.back();
+  window.history.back()
 }
 
 // Wait for the DOM content to be loaded before attaching the event listener to the back button
 document.addEventListener('DOMContentLoaded', function () {
   // Attach an event listener to the go back button to execute the function go_back
-  document.getElementById('back-btn').addEventListener('click', goBack);
-});
+  document.getElementById('back-btn').addEventListener('click', goBack)
+})
