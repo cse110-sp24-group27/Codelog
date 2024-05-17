25a40039363b4e1c8888acc2c2ae38c1a3f88fc5
Kristhian Ortiz <147196544+klortiz13@users.noreply.github.com>
2024-05-16 18:11:49 -0700

    a

--- journal.js
+++ journal.js
@@ -1,11 +1,10 @@
 // When clicked the arrow button, go back to last page
-function goBack(){
-    console.log('Go back');
-    window.history.back();
+function goBack () {
+  window.history.back();
 }
 
 // Wait for the DOM content to be loaded before attaching the event listener to the back button
-document.addEventListener("DOMContentLoaded", function() {
-    // Attach an event listener to the go back button to execute the function go_back
-    document.getElementById("back-btn").addEventListener("click", goBack);
+document.addEventListener('DOMContentLoaded', function () {
+  // Attach an event listener to the go back button to execute the function go_back
+  document.getElementById('back-btn').addEventListener('click', goBack);
 });
