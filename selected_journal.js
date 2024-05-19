aabb671f6998f4cb9a8c01beb57e000e64f111fe
Kristhian Ortiz <147196544+klortiz13@users.noreply.github.com>
2024-05-19 11:54:38 -0700

    a

--- selected_journal.js
+++ selected_journal.js
@@ -9,14 +9,14 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('back-btn').addEventListener('click', goBack)
 })
 
-// Fetch Journal Data from localStorage & Display it in The Selected Journal Page //
+// Fetch Journal Data from localStorage (onLoad) & Display it in The Selected Journal Page //
 
 // Fetch journal title
 function getTitleToDisplay () {
   // Fetch title from localStorage
   var textTitle = localStorage.getItem("journal-title");
   // Assign it to the selected journal page for display
-  document.getElementById("journal-title").innerHTML = textTitle
+  document.getElementById("journal-title").innerHTML = "put the title here using textTitle"
 }
 
 // Fetch tags
