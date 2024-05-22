fc56d1a0369d75dfea557660c93d4e8ea8c12508
Timothy <w4lam@ucsd.edu>
2024-05-21 17:31:31 -0700

    created home_projects_page.js for project home page

--- source/assets/scripts/home_projects_page.js
+++ source/assets/scripts/home_projects_page.js
@@ -1,12 +1,22 @@
 function newProject () {
   // show the pop-up after clicking + button
-  const form = document.querySelector('.new-project');
-  form.style.display = (form.style.display === 'flex' ? 'none' : 'flex');
+  const form = document.querySelector('.new-project');;
+  form.style.display = (form.style.display === 'flex' ? 'none' : 'flex');;
+}
+
+const newProjectButton = document.querySelector('#create-btn');;
+if (newProjectButton) {
+  newProjectButton.addEventListener('click', newProject);;
 }
 
 function addProject () {
   // hide the pop-up after clicking done button
-  document.querySelector('.new-project').style.display = 'none';
+  document.querySelector('.new-project').style.display = 'none';;
+}
+
+const addProjectButton = document.querySelector('#done');;
+if (addProjectButton) {
+    addProjectButton.addEventListener('clicl', addProject);;
 }
 
 // create project class or DOM for creating new project box
