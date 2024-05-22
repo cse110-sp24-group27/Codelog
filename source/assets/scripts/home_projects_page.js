16b7d70ad3f013bcd9f97546d6115917be07b1e3
Timothy <w4lam@ucsd.edu>
2024-05-22 13:10:46 -0700

    created home_projects_page.js

--- source/assets/scripts/home_projects_page.js
+++ source/assets/scripts/home_projects_page.js
@@ -1,3 +1,7 @@
+/**
+ * Event listener for + button.
+ * When clicking on the + button, pops up the form for manually create new project.
+ */
 function newProject () {
   // show the pop-up after clicking + button
   const form = document.querySelector('.new-project')
@@ -7,19 +11,23 @@ function newProject () {
 // add add event listener for + button
 const newProjectButton = document.querySelector('#create-btn')
 if (newProjectButton) {
-  newProjectButton.addEventListener('click', newProject)
+newProjectButton.addEventListener('click', newProject)
 }
 
+/**
+* Event listener for Done button in the input form.
+* When clicking on the Done button, submit the form and hide the form.
+*/
 function addProject () {
-  // hide the pop-up after clicking done button
-  document.querySelector('.new-project').style.display = 'none'
+// hide the pop-up after clicking done button
+document.querySelector('.new-project').style.display = 'none'
 }
 
 // add event listener for Done button
 // modify this to submit the form later on
 const addProjectButton = document.querySelector('#done')
 if (addProjectButton) {
-  addProjectButton.addEventListener('click', addProject)
+addProjectButton.addEventListener('click', addProject)
 }
 
 // create project class or DOM for creating new project box
