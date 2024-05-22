9802166af270bf707c13f97b46119e53d12f8322
Timothy <w4lam@ucsd.edu>
2024-05-21 17:23:29 -0700

    created home_projects_page.js for project home page

--- source/assets/scripts/home_projects_page.js
+++ source/assets/scripts/home_projects_page.js
@@ -1,12 +1,12 @@
-function new_project() {
-    // show the pop-up after clicking + button
-    const form = document.querySelector('.new-project');
-    form.style.display = (form.style.display === 'flex' ? 'none' : 'flex');
+function newProject () {
+  // show the pop-up after clicking + button
+  const form = document.querySelector('.new-project');
+  form.style.display = (form.style.display === 'flex' ? 'none' : 'flex');
 }
 
-function add_project() {
-    // hide the pop-up after clicking done button
-    document.querySelector('.new-project').style.display = 'none';
+function addProject () {
+  // hide the pop-up after clicking done button
+  document.querySelector('.new-project').style.display = 'none';
 }
 
 // create project class or DOM for creating new project box
