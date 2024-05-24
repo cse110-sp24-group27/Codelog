90741b038d2e3f5be6770770076a458be06c2b8a
yfkuo <yfkuo@ucsd.edu>
2024-05-23 19:26:58 -0700

    fixed indentation errors

--- source/reference/+_button_selcted_project_page/+_button_selcted_project_page.js
+++
@@ -1,9 +0,0 @@
-// Function to show pop-up forms
-function openPopup(popupId) {
-    document.getElementById(popupId).style.display = 'block';
-}
-
-// Function to close pop-up forms
-function closePopup(popupId) {
-    document.getElementById(popupId).style.display = 'none';
-}
