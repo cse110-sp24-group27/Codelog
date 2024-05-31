c14734782ac3e4307ee5ab87661bdfad9a8864b5
j6villanueva <112349401+j6villanueva@users.noreply.github.com>
2024-05-31 15:23:44 -0700

    fix format

--- source/tests/e2e.test.js
+++ source/tests/e2e.test.js
@@ -1,14 +1,15 @@
 // Tests that cover basic User interactions
 //First visit journal webpoage
-describe('Basic user flow for Website', () => {
-  beforeAll(async () => {
-      await page.goto('https://cse110-sp24-group27.github.io/cse110-sp24-group27/home_projects_page.html');
-  });
+// describe('Basic user flow for Website', () => {
+//   beforeAll(async () => {
+//       await page.goto('https://cse110-sp24-group27.github.io/cse110-sp24-group27/home_projects_page.html');
+//   });
   // Test Example
   test('example test', () => {
     expect(1 + 2).toBe(3)
   })
-});
+// });
+
 // Journal Entries
 // 1. Add journal entry
 // 2. Edit journal entry
