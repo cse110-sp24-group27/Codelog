6e231d4b80d50ab20465bd8368c35a1d7e338233
Kristhian Ortiz <147196544+klortiz13@users.noreply.github.com>
2024-06-01 14:34:06 -0700

    fixed styling and typos

--- source/assets/scripts/get_set_from_localStorage.js
+++ source/assets/scripts/get_set_from_localStorage.js
@@ -6,9 +6,8 @@
 // (1) home_projects_page functions //
 /**
  * Add given project object to the "user_projects" array
- * @param project - project object to be added.
+ * @param projectToAdd - project object to be added.
  * Project object has the following elements:
- * 
  *      "project_id": int starts with "current_max_project_id" + 1
  *      "projectName": "string"
  *      "description": "string"
@@ -18,45 +17,43 @@
  *              "tag_id": int that starts with ("current_max_tag_id" + 1),
  *              "tag_name": "HTML",
  *              "color": "red"
- *          }
- *      ] 
+ *          }]
  *      "selected_project_entries": {
  *          // contains all the entries shown below (!!) //
  *      }
 */
-function addProject(project_to_add) {
-    // TODO: Add given project to the "user_projects" array in localStorage
-    //Get the current "user_projects" array, or return an empty array if there is empty.
-    let projectInArray = JSON.parse(localStorage.getItem('user_projects') || '[]')
+function addProject (projectToAdd) {
+  // TODO: Add given project to the "user_projects" array in localStorage
+  // Get the current "user_projects" array, or return an empty array if there is empty.
+  const projectInArray = JSON.parse(localStorage.getItem('user_projects') || '[]')
 
-    //Add the user additions to the "user_projects" array.
-    projectInArray.push(project_to_add)
+  // Add the user additions to the "user_projects" array.
+  projectInArray.push(projectToAdd)
 
-    //Update to laocalStorage
-    localStorage.setItem('user_projects', JSON.stringify(projectInArray))
+  // Update to laocalStorage
+  localStorage.setItem('user_projects', JSON.stringify(projectInArray))
 }
 
 /**
  * Retrieve all user projects
  * @return the array "user_projects"
 */
-function getAllUserProjects() {
-    // TODO: return "user_projects" array object from localStorage
+function getAllUserProjects () {
+  // TODO: return "user_projects" array object from localStorage
 
-    //Get "user_projects" array object from localStorage
-    const projects = JSON.parse(localStorage.getItem('user_projects') || '[]')
+  // Get "user_projects" array object from localStorage
+  const projects = JSON.parse(localStorage.getItem('user_projects') || '[]')
 
-    //Retrun "user_projects" array object
-    return projects;
+  // Retrun "user_projects" array object
+  return projects
 }
 
 // (2) selected_project_page functions //
 /**
  * Add entry_to_add object to the "selected_project_entries" array of selected_project
- * @param selected_project - selected project to add the entry to
- * @param entry_to_add - entry object to be added
+ * @param selectedProject - selected project to add the entry to
+ * @param entryToAdd - entry object to be added
  * (!!) Entry object has the following elements:
- * 
  *      "entry_id": int that starts with ("current_max_entry_id" + 1) -> don't forget to update current_max_entry_id
  *      "titleName": "string"
  *      "description": "string"
@@ -70,8 +67,7 @@ function getAllUserProjects() {
  *              "tag_id": 30002,
  *              "tag_name": "HTML",
  *              "color": "red"
- *          }
- *      ] 
+ *          }] 
  *      "publicity": "string"
  *      "content": [
  *          {
@@ -85,60 +81,59 @@ function getAllUserProjects() {
  *          {
  *              "type": "text"
  *              "content": "text content"
- *          }
- *      ]
+ *          }]
 */
-function addEntry(selected_project, entry_to_add) {
-    // TODO: Add given entry to the "selected_project_entries" array of corresponding project in localStorage
-    //get the max entry id    
-    let current_max_entry_id = selected_project.current_max_entry_id;
+function addEntry (selectedProject, entryToAdd) {
+  // TODO: Add given entry to the "selected_project_entries" array of corresponding project in localStorage
+  // get the max entry id    
+  let current_max_entry_id = selectedProject.current_max_entry_id
 
-    //set the entry id to current_max_entry_id + 1
-    entry_to_add.entry_id = current_max_entry_id +1;
+  // set the entry id to current_max_entry_id + 1
+  entryToAdd.entry_id = current_max_entry_id +1
 
-    //set the "current_max_entry_id" to "entry_to_add.entry_id"
-    selected_project.current_max_entry_id = entry_to_add.entry_id;
+  // set the "current_max_entry_id" to "entry_to_add.entry_id"
+  selectedProject.current_max_entry_id = entryToAdd.entry_id
 
-    //push the given entry to "selected_project_entries" array
-    selected_project.selected_project_entries.push(entry_to_add);
+  // push the given entry to "selected_project_entries" array
+  selectedProject.selected_project_entries.push(entryToAdd)
 
-    //update to localStorage
-    localStorage.setItem('selected_project', JSON.stringify(selected_project));
+  // update to localStorage
+  localStorage.setItem('selected_project', JSON.stringify(selectedProject))
 }
 
 /**
  * Retrieve all entries of the selected project given the project id
  * @param projects - object `user_projects`
- * @param project_id - int, id of the project
+ * @param projectId - int, id of the project
  * @return the "selected_project_entries" array of that selected project
 */
-function getAllSelectedProjectEntries(projects, project_id) {
-    // TODO: return "selected_project_entries" array of the given project from localStorage
+function getAllSelectedProjectEntries (projects, projectId) {
+  // TODO: return "selected_project_entries" array of the given project from localStorage
 
-    //Iterate through projects, find the project with matching project_id, then return that project's entries
-    projects.forEach(project => {
-        if (project.project_id == project_id) {
-            return project.selected_project_entries
-        }
-    })
+  // Iterate through projects, find the project with matching project_id, then return that project's entries
+  projects.forEach(project => {
+    if (project.projectId === projectId) {
+        return project.selected_project_entries
+    }
+  })
 
-    //If no project with the correct project_id found from above loop, console.log
-    console.log(`There's no project found with ${project_id}`)
+  // If no project with the correct project_id found from above loop, console.log
+  console.log(`There's no project found with ${projectId}`)
 }
 
 // (3) selected_journal_page functions //
 /**
- * @param selected_project - selected project to grab the entry from
- * @param entry_id - the id of the entry we want to display in selected_journal_page.js
+ * @param selectedProject - selected project to grab the entry from
+ * @param entryId - the id of the entry we want to display in selected_journal_page.js
  * @return entry given an id
 */
-function getSelectedEntry(selected_project, entry_id) {
-    // TODO: return the specific entry of the given project from localStorage
-    let entries = selected_project.selected_project_entries
-    entries.forEach(entry => {
-        if (entry.entry_id == entry_id) {
-            return entry
-        }
-    })
-    console.log(`No entry with entry_id ${entry_id} found in selected_project`)
+function getSelectedEntry (selectedProject, entryId) {
+  // TODO: return the specific entry of the given project from localStorage
+  let entries = selectedProject.selected_project_entries
+  entries.forEach(entry => {
+    if (entry.entryId === entryId) {
+        return entry
+    }
+  })
+  console.log(`No entry with entry_id ${entryId} found in selected_project`)
 }
