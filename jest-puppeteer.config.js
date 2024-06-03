293fe688bf5d4b14d8bfc57bf492101bb9ef4f7d
j6villanueva <112349401+j6villanueva@users.noreply.github.com>
2024-06-03 13:20:02 -0700

    remove comma

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,7 +2,7 @@ module.exports = {
   launch: {
     headless: true,
     slowMo: 25,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   }
 }
 
