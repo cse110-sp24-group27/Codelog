48128df2e07c8859edfe218b6c81d8c3f3608855
Tim0727 <timlam727@gmail.com>
2024-06-02 17:51:05 -0700

    added function for dragging project

--- source/assets/scripts/main.js
+++ source/assets/scripts/main.js
@@ -120,6 +120,69 @@ function addProjectsToDocument (projects) {
     projectCard.data = project
     mainElement.appendChild(projectCard)
   }
+  dragProjects()
+}
+
+/**
+ * Function that allows user to drag the project and reorder
+ * projects.
+ * Unfinished!
+ */
+function dragProjects() {
+  const projects = document.querySelector('.projects')
+  const projectCards = projects.querySelectorAll('project-card')
+  projectCards.forEach(project => {
+    const article = project.shadowRoot.querySelector('.project')
+    project.addEventListener('dragstart', () => {
+      // Adding dragging class to project after a delay
+      setTimeout(() => article.classList.add('dragging'))
+    })
+    // Removing dragging-project class from project on dragend event
+    project.addEventListener('dragend', () => article.classList.remove('dragging'))
+  })
+
+  let draggingItem = null;
+  const initProjects = (e) => {
+    if (!draggingItem) {
+      projectCards.forEach(project => {
+        if (project.shadowRoot.querySelector('.project').classList.contains('dragging')) {
+          draggingItem = project;
+        }
+      })
+    }
+    if (!draggingItem) return
+    projectCards.forEach(project => {
+      if (project.shadowRoot.querySelector('.project').classList.contains('dragging')) {
+        draggingItem = project;
+      }
+    })
+    console.log(draggingItem)
+    // Getting all items except currently dragging and making array of them
+    const siblings = []
+    projectCards.forEach(project => {3
+      const article = project.shadowRoot.querySelector('.project')
+      if (!article.classList.contains('dragging')) {
+        siblings.push(project)
+      }
+    })
+
+    console.log(siblings)
+    // Finding the sibling after which teh dragging item should be placed
+    let nextSibling = siblings.find(sibling => {
+      console.log(sibling.offsetTop)
+      console.log(sibling.offsetWidth)
+      const rect = sibling.getBoundingClientRect()
+      return e.clientY <= rect.top + rect.height / 2;
+    })
+    console.log(nextSibling)
+    // Inserting the dragging project before the found sibling
+    if (nextSibling) {
+      projects.insertBefore(draggingItem, nextSibling)
+    } else if (e.clientY > siblings[siblings.length - 1].getBoundingClientRect().bottom) {
+      projects.appendChild(draggingItem)
+    }
+  }
+  projects.addEventListener('dragover', initProjects)
 }
 
 // export functions for testing
