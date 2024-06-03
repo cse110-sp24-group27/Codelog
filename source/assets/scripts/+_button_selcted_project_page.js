5ccdc9e63cdc797be3af0d1ddb984feb52cad249
yfkuo <yfkuo@ucsd.edu>
2024-06-03 00:40:20 -0700

    journal entry creator: implementions for html css js to align with new design updates

--- source/assets/scripts/+_button_selcted_project_page.js
+++ source/assets/scripts/+_button_selcted_project_page.js
@@ -8,5 +8,38 @@ function closePopup (id) {
   document.getElementById(id).style.display = 'none'
 }
 
+// Function to add a new input field and delete button
+function addInput(columnNumber) {
+  // Get the container where the new input group will be added
+  const container = document.getElementById('inputContainer')
+  // Create a new div element to act as the input group
+  const inputGroup = document.createElement('div')
+  inputGroup.classList.add('input-group')
+  // Create a new input element
+  const newInput = document.createElement('input')
+  newInput.type = 'text'
+  newInput.placeholder = `Input for ${columnNumber}`
+  newInput.classList.add(`input-${columnNumber}`)
+  // Create a new button element for deletion
+  const deleteButton = document.createElement('button')
+  deleteButton.classList.add('delete-btn')
+  deleteButton.innerText = 'Delete'
+  // Add an onclick event to the delete button to remove the input group
+  deleteButton.onclick = function() {
+    deleteInput(deleteButton)
+  }
+  // Add an onclick event to the delete button to remove the input group
+  inputGroup.appendChild(newInput)
+  inputGroup.appendChild(deleteButton)
+  container.appendChild(inputGroup)
+}
+// Function to delete an input group
+function deleteInput(button) {
+  const inputGroup = button.parentElement
+  inputGroup.remove()
+}
+
 window.openPopup = openPopup
 window.closePopup = closePopup
+window.addInput = addInput
+window.deleteInput = deleteInput
