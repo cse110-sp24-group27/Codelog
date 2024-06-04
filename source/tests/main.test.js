92d3b959b8a8f5f5007bba5be621477f8e4a0aeb
shootingdarts <103202799+shootingdarts@users.noreply.github.com>
2024-06-03 21:36:22 -0700

    Added a temp test

--- source/tests/main.test.js
+++ source/tests/main.test.js
@@ -5,17 +5,21 @@
 const { getProjectsFromStorage } = require('../assets/scripts/main')
 const project1 = ``
 
-/* 
 describe('Testing getProjectsFromStorage...', () => {
-  window.localStorage.setItem('projects', JSON.stringify([project1]))
+  // window.localStorage.setItem('projects', JSON.stringify([project1]))
   const projectArr = getProjectsFromStorage()
+  test('Testing get projects when no project...', () => {
+    expect(projectArr.length).toEqual(0)
+  })
+  /*
   test('Testing get projects when one project was added...', () => {
     expect(projectArr.length).toEqual(1)
   })
   test('Testing output array content...', () => {
     expect(projectArr[0]).toEqual(project1)
   })
+  */
 })
-*/
+
 
 
