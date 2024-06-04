da07f6bb9ceeadff9cd65824254de53373cfdeb5
shootingdarts <103202799+shootingdarts@users.noreply.github.com>
2024-06-03 21:32:59 -0700

    Removed two tests

--- source/tests/main.test.js
+++ source/tests/main.test.js
@@ -3,37 +3,9 @@
  */
 
 const { getProjectsFromStorage } = require('../assets/scripts/main')
-const project1 = `{
-    "projectId": 1,
-    "projectName": "Project Awesome",
-    "projectDescription": "This is a description for an awesome project. It's a long description to fill up some space and make the project card look more interesting.",
-    "status": "Public",
-    "tags": ["Awesome", "Web Dev", "JavaScript"],
-    "journals": [
-      {
-        "journalId": 1,
-        "journalTitle": "Journal Entry 1 for Project Awesome",
-        "tags": ["Planning", "Initial Research"],
-        "template": "This is some content using a template",
-        "markdown": "This is some markdown content for the journal entry."
-      },
-      {
-        "journalId": 2,
-        "journalTitle": "Journal Entry 2 for Project Awesome",
-        "tags": ["Development", "Progress Update"],
-        "template": "This is some other template content",
-        "markdown": "Here's more markdown content for another journal entry."
-      },
-      {
-        "journalId": 3,
-        "journalTitle": "Journal Entry 3 for Project Awesome",
-        "tags": ["Testing", "Bug Fixes"],
-        "template": "Even more template content!",
-        "markdown": "And some final markdown content for this project."
-      }
-    ]
-  }`
+const project1 = ``
 
+/* 
 describe('Testing getProjectsFromStorage...', () => {
   window.localStorage.setItem('projects', JSON.stringify([project1]))
   const projectArr = getProjectsFromStorage()
@@ -44,3 +16,6 @@ describe('Testing getProjectsFromStorage...', () => {
     expect(projectArr[0]).toEqual(project1)
   })
 })
+*/
+
+
