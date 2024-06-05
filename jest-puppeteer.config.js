d7f3f00a83b9c44c566602cf2dcebac7cc38fd05
shootingdarts <103202799+shootingdarts@users.noreply.github.com>
2024-06-04 22:31:49 -0700

    Fixing pptr error

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   launch: {
+    args: ['--no-sandbox'],
+    executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
     headless: false,
-    slowMo: 25,
   }
 }
