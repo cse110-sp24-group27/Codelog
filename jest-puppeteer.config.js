22d7637ab7ddcd57b32b1839ded0a81b59f095cb
shootingdarts <103202799+shootingdarts@users.noreply.github.com>
2024-06-04 22:31:15 -0700

    Fixing pptr error

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   launch: {
-    headless: process.env.CI === "true",
-    ignoreDefaultArgs: ["--disable-extensions"],
-    args: ["--no-sandbox"],
-    executablePath: "chrome.exe"
+    headless: false,
+    slowMo: 25,
   }
 }
