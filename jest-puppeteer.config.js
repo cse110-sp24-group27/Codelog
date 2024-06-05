4dbaa6722cc8a8880dc7c334be03e4e21081e101
shootingdarts <103202799+shootingdarts@users.noreply.github.com>
2024-06-04 22:37:01 -0700

    Testing changes to yml

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   launch: {
-    args: ['--no-sandbox'],
-    executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
     headless: false,
+    slowMo: 25,
   }
 }
