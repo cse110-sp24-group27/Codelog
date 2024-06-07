f04faf263a9a3192207851b13ddbada59131a817
shootingdarts <103202799+shootingdarts@users.noreply.github.com>
2024-06-07 01:49:06 -0700

    Fixed tests for home_projects_page

--- source/tests/home_projects_page.unit.js
+++ source/tests/home_projects_page.unit.js
@@ -3,26 +3,12 @@
  */
 
 const { getUnusedProjectId, addProjectToLocalStorage } = require('../assets/scripts/home_projects_page.js')
-const ele1 = document.createElement('div')
-const ele2 = document.createElement('div')
-const container = document.createElement('div')
-const container2 = document.createElement('div')
-const button = document.createElement('button')
-ele1.setAttribute('id', 'test1')
-ele2.setAttribute('id', 'test2')
-container.setAttribute('id', 'inputcontainer')
-container2.setAttribute('id', 'inputcontainer2')
-container2.appendChild(button)
-document.body.appendChild(ele1)
-document.body.appendChild(ele2)
-document.body.appendChild(container)
-document.body.appendChild(container2)
 
 describe('Testing getUnusedProjectId...', () => {
   const id = 1
-  window.localStorage.setItem('current_max_project_id', JSON.stringify(id))
+  window.localStorage.setItem('currentMaxProjectId', JSON.stringify(id))
   const unusedId = getUnusedProjectId()
-  const storage = JSON.parse(window.localStorage.getItem('current_max_project_id'))
+  const storage = JSON.parse(window.localStorage.getItem('currentMaxProjectId'))
   test('Testing if localStorage is changed...', () => {
     expect(storage).toEqual(id + 1)
   })
@@ -33,13 +19,13 @@ describe('Testing getUnusedProjectId...', () => {
 
 describe('Testing addProjectToLocalStorage...', () => {
   const id = 1
-  window.localStorage.setItem('current_max_project_id', JSON.stringify(id))
+  window.localStorage.setItem('currentMaxProjectId', JSON.stringify(id))
   addProjectToLocalStorage({})
   const storage = JSON.parse(window.localStorage.getItem('user_projects'))
   test('Testing if localStorage is changed...', () => {
     expect(storage.length).toEqual(1)
   })
   test('Testing if project_id is correct...', () => {
-    expect(storage[0].project_id).toEqual(id + 1)
+    expect(storage[0].projectId).toEqual(id + 1)
   })
 })
