aadee0ed97239212b4066005648d3ad070cc4b87
shootingdarts <103202799+shootingdarts@users.noreply.github.com>
2024-06-07 02:46:34 -0700

    Added more tests to profile_setting

--- source/tests/profile_setting.unit.js
+++ source/tests/profile_setting.unit.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { getProfileFromStorage, updateProfileOnPage } = require('../assets/scripts/main.js')
+const { getProfileFromStorage, updateProfileOnPage, save } = require('../assets/scripts/profile_setting.js')
 
 // Setting up Dom for testing
 const image = document.createElement('img')
@@ -12,6 +12,7 @@ const description = document.createElement('div')
 const email = document.createElement('a')
 const linkedin = document.createElement('a')
 const github = document.createElement('a')
+const pfp = document.createElement('div')
 image.setAttribute('id', 'profile-picture')
 name.setAttribute('id', 'name')
 pronoun.setAttribute('id', 'pronoun')
@@ -19,6 +20,7 @@ description.setAttribute('id', 'description')
 email.setAttribute('id', 'link-email')
 linkedin.setAttribute('id', 'link-linkedin')
 github.setAttribute('id', 'link-github')
+pfp.setAttribute('id', 'profile-picture-upload')
 document.body.appendChild(image)
 document.body.appendChild(name)
 document.body.appendChild(pronoun)
@@ -26,21 +28,23 @@ document.body.appendChild(description)
 document.body.appendChild(email)
 document.body.appendChild(linkedin)
 document.body.appendChild(github)
+document.body.appendChild(pfp)
 const userProfile = {
+  profilePicture: 'https://via.placeholder.com/150',
   username: 'David',
   pronouns: 'He/Him',
   bio: 'desc',
   socialLinks: {
     email: 'https://via.placeholder.com/1140',
-    linkedin: 'https://via.placeholder.com/1160',
+    linkedin: 'https://www.via.placeholder.com/1160',
     github: 'https://via.placeholder.com/1170'
   }
 }
 
 describe('Testing getProfileFromStorage...', () => {
   const profileArr = getProfileFromStorage()
-  test('Testing get projects when no project...', () => {
-    expect(profileArr.length).toEqual(0)
+  test('Testing get projects when no projects...', () => {
+    expect(profileArr).toEqual(null)
   })
 })
 
@@ -50,21 +54,28 @@ describe('Testing updateProfileOnPage...', () => {
     expect(image.src).toEqual('https://via.placeholder.com/150')
   })
   test('Testing username...', () => {
-    expect(name.textContent).toEqual('David')
+    expect(name.value).toEqual('David')
   })
   test('Testing pronouns...', () => {
-    expect(pronoun.textContent).toEqual('He/Him')
+    expect(pronoun.value).toEqual('He/Him')
   })
   test('Testing description...', () => {
-    expect(description.textContent).toEqual('desc')
+    expect(description.value).toEqual('desc')
   })
   test('Testing email link...', () => {
     expect(email.href).toEqual('mailto:https://via.placeholder.com/1140')
   })
   test('Testing linkedin link...', () => {
-    expect(linkedin.href).toEqual('https://via.placeholder.com/1160')
+    expect(linkedin.href).toEqual('https://www.via.placeholder.com/1160')
   })
   test('Testing github link...', () => {
     expect(github.href).toEqual('https://via.placeholder.com/1170')
+  })
+  window.localStorage.setItem('user_profile', JSON.stringify({ profilePicture: 'https://via.placeholder.com/150' }))
+  window.alert = () => {}
+  save()
+  const storage = JSON.parse(window.localStorage.getItem('user_profile'))
+  test('Testing if profile is saved to localStorage...', () => {
+    expect(storage).toEqual(userProfile)
   })
 })
